f91cf6a551b549e7d20ee039bf82def84d406f36
Baran Baygan <baran@rettermobile.com>
2022-10-18 13:34:25 +0300

    initial

---
+++ app.js
@@ -0,0 +1,11 @@
+
+console.log("COLD START")
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
