eae634ea50d2bb5d00d9f3851023798b10dad5f8
Baran Baygan <baran@rettermobile.com>
2022-10-26 10:15:36 +0300

    yeni container

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ exports.handler = async (event) => {
     // TODO implement
     const response = {
         statusCode: 200,
-        body: JSON.stringify('Hello from custom docker container!'),
+        body: JSON.stringify('Hello from custom docker container 2222!'),
     };
     return response;
 };
